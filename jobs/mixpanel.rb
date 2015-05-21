9ac913f4de9d4993b8d4fedba9fdbbeb8564ccd5
Rutger van Waveren <rutger@thequestionmark.org>
2015-05-21 11:18:05 +0200

    fixed bug in mixpanel widget
    - also removed pontje to an inactive job

--- jobs/mixpanel.rb
+++ jobs/mixpanel.rb
@@ -5,26 +5,26 @@ root = ::File.dirname(__FILE__)
 require ::File.join(root, "..", 'lib', 'mixpanel_config')
 require ::File.join(root, "..", 'lib', 'mixpanel_event_number')
  
-def mixpanel_exports
-  send_event('submit_product', {
-    current: number_for_event_using_export("Aantal toegevoegde producten")
-  })
+#def mixpanel_exports
+#  send_event('submit_product', {
+#     current: number_for_event_using_export("Ingezonden producten", "Netherlands", true)
+#   })
  
 #  send_event('mixpanel_signup_portland', {
 #    current: number_for_event_using_export("Sign Up", "portland", true)
 #  })
  
-end
+#end
  
 ####### get updates from MixPanel ##########
-SCHEDULER.every '1m', :first_in => 0 do |job|
+#SCHEDULER.every '24h', :first_in => 0 do |job|
   # MixPanel export API doesn't allow for concurrent requests to export API, so need to make the sequential 
-  mixpanel_exports
-end
+#  mixpanel_exports
+#end
  
-SCHEDULER.every '1m', :first_in => 0 do |job|
-  send_event('submit_product', {
-    current: mixpanel_event_number(event_name: "Aantal toegevoegde producten")
+SCHEDULER.every '10s', :first_in => 0 do |job|
+  send_event('m-submit_product', {
+    current: mixpanel_event_number(event_name: "submit_product")
   })
  
 end

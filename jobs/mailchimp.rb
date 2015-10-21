5098d7eb43648ba07d628db4a5c68223ca6e8966
Rutger van Waveren <rutger@thequestionmark.org>
2015-10-21 18:25:24 +0200

    back to old Gibbon gem
    mailchimp widget not working now…

--- jobs/mailchimp.rb
+++ jobs/mailchimp.rb
@@ -1,3 +1,5 @@
+=begin
+
 require 'gibbon'
 
 apikey = ENV['MAILCHIMP_API_KEY']
@@ -10,4 +12,18 @@ if apikey
       current: newsletter['stats']['member_count']
     })
   end
+end
+
+=end
+
+
+require 'gibbon'
+
+SCHEDULER.every '5m', first_in: 0 do |job|
+  gibbon = Gibbon::API.new
+  gibbon.api_key = ENV['MAILCHIMP_API_KEY']
+  newsletter = gibbon.lists.list
+  send_event('mailchimp', {
+    current: newsletter['data'][19]['stats']['member_count']
+  })
 end

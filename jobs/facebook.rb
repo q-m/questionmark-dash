1e54554ab85485d5b77ff9cecc8db7c0cf466eaa
wvengen <willem@thequestionmark.org>
2016-10-04 09:48:32 +0200

    Fix Facebook job

--- jobs/facebook.rb
+++ jobs/facebook.rb
@@ -4,12 +4,20 @@ require 'json'
 pages = ['questionmarkfoundation']
 urls  = ['http://www.thequestionmark.org/']
 
+metrics = {
+  'shares' => lambda {|d| d['share'] && d['share']['share_count']},
+  'talking_about_count' => lambda {|d| d['talking_about_count']},
+  'likes' => lambda {|d| d['likes']},
+}
+
 SCHEDULER.every '24h', :first_in => 0 do |job|
   open 'https://graph.facebook.com/?ids=' + (pages+urls).join(',') do |f|
     result = JSON.parse(f.read)
 
-    %w(shares talking_abount_count likes).each do |what|
-      value = result.map{|url, data| data[what]}.compact.reduce(&:+)
+    metrics.each_pair do |what, getter|
+      values = result.map{|url, data| getter.call(data)}
+      value = values.to_a.compact.reduce(&:+)
+
       send_event("facebook-#{what}", {current: value})
     end
   end
